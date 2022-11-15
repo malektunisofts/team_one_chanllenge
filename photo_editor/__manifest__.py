26b76dd0ac4fa3b4af2e1ffa72863a83bb265a12
DESKTOP-KE00NP4\TSF0050 <mas@tunisofts.com>
2022-11-15 10:52:07 +0100

    first commit

---
+++ photo_editor/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Photo Editor',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'summary': 'Photo editor',
+    'description': "",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
